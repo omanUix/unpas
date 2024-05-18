765f67c07c808f2fdf8aa1d65e6e36ca7357a963
omanUix <rahmanabdu702@gmail.com>
2024-05-18 21:32:22 +0700

    Saving Last Studying yet?

---
+++ flexBox/ngobar/script.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector('.toggle-menu input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function () {
+    nav.classList.toggle('slide');
+});
